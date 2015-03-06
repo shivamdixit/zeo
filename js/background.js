b3783248f2a07270a4d1015f8d01ac20ce347823
Shivam Dixit <shivamd001@gmail.com>
2015-03-06 09:50:15 +0530

    Removed background.js from version control
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- js/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        chrome.browserAction.setIcon({
-            path: request.newIconPath,
-            tabId: sender.tab.id
-    });
-})
